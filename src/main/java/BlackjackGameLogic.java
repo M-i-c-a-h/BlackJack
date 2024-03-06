8a2cb499a6c272ada7dfe7c84da280362410ad7a
M-i-c-a-h <oolug4@uic.edu>
2024-03-06 02:41:16 -0600

    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/BlackjackGameLogic.java
@@ -0,0 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * BlackjackGameLogic - implements game logic
+ * @method + String whoWon(ArrayList <Card> playerHand1, ArrayList <Card> dealerHand)
+ * @method + int handTotal(ArrayList <Card> hand)
+ * @method + boolean evaluateBankerDraw(ArrayList <Card> banker)
+ */
+public class BlackjackGameLogic {
+    /**
+     *
+     * @param playerHand1 - ArrayList of player1's cards
+     * @param dealerHand - ArrayList of dealer's cards
+     * @return player with sum closest to sum to 21 or draw if both players have equal sum
+     */
+    String whoWon(ArrayList<Card> playerHand1, ArrayList <Card> dealerHand){
+        int player1_total = this.handTotal(playerHand1);
+        int dealer_total = this.handTotal(dealerHand);
+
+        // if player and dealer have equal scores, game is tied, return push
+        if(player1_total == dealer_total){ return "push";}
+
+        // if player has total less than or equal to 21 and greater than dealer, player wins
+        if((player1_total <= 21) && (dealer_total >= 22 || (player1_total > dealer_total))) {
+            return "player";
+        }
+
+        return "dealer"; // Dealer wins if player busts or dealer's total is higher
+    }
+
+    /**
+     * handTotal() returns the sum of card values in ArrayList
+     * @param hand - ArrayList of player's cards
+     * @return sum of Card value in ArrayList
+     */
+    int handTotal(ArrayList <Card> hand){
+        int sum = 0;
+        for(Card card : hand){
+            sum += card.value;
+        }
+        return sum;
+    }
+
+    /**
+     * evaluateBankerDraw() - determines if baker is required to draw at current round
+     * @param banker  ArrayList of banker's card
+     * @return true if the dealer should draw another card, i.e. if the value is 16 or less.
+     */
+    boolean evaluateBankerDraw(ArrayList <Card> banker){
+        return (this.handTotal(banker) <= 16);
+    }
+};
---
+++ src/main/java/Card.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+/**
+ * Card class creates a card object
+ * @method + Card(String theSuit, int theValue)
+ * @attribute + string suit - card type (club, spade, diamond, lotus) and face
+ * @attribute + int value - numeric val of card (1-10)
+ */
+public class Card {
+
+    String suit;
+    int value;
+
+    /**
+     * Card constructor(string, int) creates new Card object
+     * @param theSuit - card suit
+     * @param theValue - card value
+     */
+    Card(String theSuit, int theValue){
+        this.suit = theSuit;
+        this.value = theValue;
+    }
+};
+
